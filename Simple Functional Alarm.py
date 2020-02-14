cf4bcb0485fead70ff60bd16b98eb1de17f6ee33
ChiefGains <48441498+ChiefGains@users.noreply.github.com>
2020-02-14 12:37:51 -0500

    Update Simple Functional Alarm.py
    corrected formatting for alarm_time and for time.strftime('')

--- Simple Functional Alarm.py
+++ Simple Functional Alarm.py
@@ -6,14 +6,14 @@ from time import strftime as now
 from playsound import playsound as play
 #imports the module to play music
 
-alarm_time = str(7:00:00)
+alarm_time = str('7:00:00')
 #sets the alarm for 7am. Can have the user set alarm using input() or with GPIO, but that falls outside of the scope of this program
 
 song = r'C:\\generic_filepath.mp3'
 #tells the program what song to play for the alarm
 
 while True:
-  if alarm_time == now:
+  if alarm_time == now('%H:%M:%S'):
     play(song)
     break
     #this loop will continually check the time until it's time for the alarm to go off,
