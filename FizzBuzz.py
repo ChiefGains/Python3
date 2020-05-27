aca790de1cc63417755b6fa896a400f1e2d4586b
ChiefGains <48441498+ChiefGains@users.noreply.github.com>
2020-05-26 22:47:30 -0400

    Update FizzBuzz.py
    Misunderstood that numbers divisible by both 3 and 5 should print "FizzBuzz," but I fixed it. Man, it was so, so hard....

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,7 +3,9 @@ from random import randint as rand
 n = rand(0,100)
 
 for i in range(n):
-    if i % 3 == 0:
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')    
+    elif i % 3 == 0:
         print('Fizz')
     elif i % 5 == 0:
         print('Buzz')
